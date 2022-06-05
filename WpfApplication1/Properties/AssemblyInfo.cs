a354e2ac34f77478126d88d8d49da734c4bc5cdd
VladiZ <vladzdravkov@gmail.com>
2022-06-05 10:58:28 +0200

    Qt Categorization fixed, Setup fixed

--- WpfApplication1/Properties/AssemblyInfo.cs
+++ WpfApplication1/Properties/AssemblyInfo.cs
@@ -51,6 +51,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.0.1")]
-[assembly: AssemblyFileVersion("1.5.0.1")]
-[assembly: NeutralResourcesLanguageAttribute("en-US")]
+[assembly: AssemblyVersion("1.6.0.1")]
+[assembly: AssemblyFileVersion("1.6.0.1")]
+[assembly: NeutralResourcesLanguage("en")]
