db156aa49a7b702a5b64bea5d7960c2fd58cde3e
VladiZ <vladzdravkov@gmail.com>
2026-03-08 11:08:19 +0100

    Look of selected 3DBar, processing of input files improved

--- WpfApplication1/DAL/PathToFilesUtil.cs
+++ WpfApplication1/DAL/PathToFilesUtil.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Linq;
 using System.Windows;
@@ -9,32 +9,38 @@ namespace WpfApplication1.DAL
     static class PathToFilesUtil
     {
         static string[] categorizedInputCSVfiles;
-        private static readonly object _fileLock;
-        static PathToFilesUtil()
-        {
-            _fileLock = new object();            
-        }
-        public static string[] GetInputRawCsvFiles()
+        static string[] nonCategorizedInputCSVfiles;
+        private static readonly object _fileLock = new object();
+
+        public static void ObtainInputRawCsvFiles()
         {
             try
             {
                 categorizedInputCSVfiles = Directory.GetFiles(Config.PathToSskaDownloadsFolder, @"*categorized.csv");
                 string[] allInputCSVfiles = Directory.GetFiles(Config.PathToSskaDownloadsFolder, @"*.csv");
-                return allInputCSVfiles.Except(categorizedInputCSVfiles).ToArray();
+                nonCategorizedInputCSVfiles = allInputCSVfiles.Except(categorizedInputCSVfiles).ToArray();
+                // move old categorized to arxive, only non-categorized csv files to be processed further
+                foreach (var categorizedCSVFile in categorizedInputCSVfiles)
+                {
+                    MoveFileToArxiv(categorizedCSVFile, "_old_arxive");
+                }
             }
             catch (Exception e)
             {
                 MessageBox.Show(e.Message, Config.AppName + ": Unable to get Target File.", MessageBoxButton.OK, MessageBoxImage.Error);
-                return null;
             }
         }
 
-        public static string GetNameInputCategorizedCsvFile()
+        public static string[] GetCategorizedInputCSVFilePaths => Directory.GetFiles(Config.PathToSskaDownloadsFolder, @"*categorized.csv");
+
+        public static string[] GetNonCategorizedInputCSVFilePaths => nonCategorizedInputCSVfiles;
+
+        public static string GetNameOfFirstInputCategorizedCsvFile()
         {
             try
             {
-                categorizedInputCSVfiles = Directory.GetFiles(Config.PathToSskaDownloadsFolder, @"*categorized.csv");
-                return categorizedInputCSVfiles.Length > 0 ? categorizedInputCSVfiles[0] : string.Empty;
+                var files = GetCategorizedInputCSVFilePaths;
+                return files.Length > 0 ? files[0] : string.Empty;
             }
             catch (Exception e)
             {
@@ -56,7 +62,6 @@ namespace WpfApplication1.DAL
                     {
                         sw.WriteLine(@"<Root>");
                         sw.WriteLine(@"</Root>");
-                        sw.Close();
                         MessageBox.Show("The necessary folders and empty arxiv file 'Arxiv.xml' have been created.", Config.AppName, MessageBoxButton.OK);
                         return pathToArxivFile;
                     }
@@ -67,11 +72,6 @@ namespace WpfApplication1.DAL
                 }
             }
             return string.Empty;
-        }
-
-        public static string GetArxivedCsvFilePath(string fileName)
-        {
-            return Path.Combine(Config.PathToXmlStorageFolder, Path.GetFileName(fileName));
         }
 
         public static string AppendSuffixToFileName(string fileName, string suffix)
@@ -85,55 +85,29 @@ namespace WpfApplication1.DAL
             string categorizedFileName = AppendSuffixToFileName(rawCsvFile, "_categorized");
             try
             {
-                FileStream fileStream = File.Create(categorizedFileName);
-                if (File.Exists(categorizedFileName))
-                {
-                    fileStream.Close();
-                }
+                using (File.Create(categorizedFileName)) { }
                 return categorizedFileName;
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Failure to create or move csv file with suffix categorized", ex.Message);
+                MessageBox.Show("Failure to create csv file with suffix categorized", ex.Message);
                 return string.Empty;
             }
         }
 
-        static void AppendCategoryFields(ref string[] sources)
-        {
-            string headers = sources.FirstOrDefault();
-            if (headers != null)
-            {
-                sources[0] = headers.Insert(headers.Length, Config.Delimiter4CSVFile + Config.CategoryIdField + Config.Delimiter4CSVFile + Config.CategoryField);
-            }
-            for (int i = 0; i < sources.Length; i++)
-            {
-                if (i > 0)
-                {
-                    sources[i] = sources[i].Insert(sources[i].Length, Config.Delimiter4CSVFile + Config.Delimiter4CSVFile);
-                }
-            }
-        }
-        public static void MoveFileToArxiv()
+        public static void MoveFileToArxiv(string pathToCSVFile, string suffix)
         {
             try
             {
                 lock (_fileLock)
                 {
                     if (!Directory.Exists(Config.PathToXmlStorageFolder))
-                    {
                         Directory.CreateDirectory(Config.PathToXmlStorageFolder);
-                    }
 
-                    string categorizedFile = GetNameInputCategorizedCsvFile();
-                    if (!string.IsNullOrEmpty(categorizedFile))
+                    if (!string.IsNullOrEmpty(pathToCSVFile))
                     {
-                        string fileName = AppendSuffixToFileName(categorizedFile, "_arxiv");
-                        for (int i = 0; File.Exists(GetArxivedCsvFilePath(fileName)); i++)
-                        {
-                            fileName = $"{fileName}_{i}";
-                        }
-                        File.Move(categorizedFile, GetArxivedCsvFilePath(fileName));
+                        string fileName = GetUniqueCsvFileName(pathToCSVFile, suffix);
+                        File.Move(pathToCSVFile, fileName);
                     }
                 }
             }
@@ -141,6 +115,20 @@ namespace WpfApplication1.DAL
             {
                 MessageBox.Show(e.ToString(), Config.AppName + ": Unable to move File in Arxiv.", MessageBoxButton.OK, MessageBoxImage.Error);
             }
+        }
+
+        public static string GetUniqueCsvFileName(string baseName, string suffix)
+        {
+            string dir = Config.PathToXmlStorageFolder;
+            string fn = Path.GetFileNameWithoutExtension(baseName);
+            string filePath = Path.Combine(dir, fn + suffix + ".csv");
+            int counter = 1;
+            while (File.Exists(filePath))
+            {
+                filePath = Path.Combine(dir, $"{fn}{suffix}_{counter}.csv");
+                counter++;
+            }
+            return filePath;
         }
     }
 }
